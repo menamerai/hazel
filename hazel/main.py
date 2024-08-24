fdb09a5e4b58e18b823d1aa98383db8795f5f104
Rai <62800649+menamerai@users.noreply.github.com>
2024-08-24 06:59:58 -0400

    Judge register (#33)
    * [fix] typo in table
    
    * [feat] judge register

--- hazel/main.py
+++ hazel/main.py
@@ -32,6 +32,7 @@ async def on_ready():
     await client.load_extension("hazel.slashcmds.hacker")
     await client.load_extension("hazel.slashcmds.mentor")
     await client.load_extension("hazel.slashcmds.judge")
+    await client.load_extension("hazel.slashcmds.sponsor")
     await client.load_extension("hazel.slashcmds.team")
     await client.load_extension("hazel.slashcmds.matchmake")
     # sync current commands with the fodh server for quick development
--- hazel/slashcmds/sponsor.py
+++ hazel/slashcmds/sponsor.py
@@ -11,20 +11,20 @@ from hazel.utils.constants import *
 from hazel.utils.users import check_if_user_exists
 
 
-class Judge(app_commands.Group):
-    @app_commands.command(description="Register as a Judge for the event")
+class Sponsor(app_commands.Group):
+    @app_commands.command(description="Register as a Sponsor for the event")
     async def register(self, interaction: discord.Interaction, password: str):
         logging.info(
-            f"Judge.register: received register request from {interaction.user}"
+            f"Sponsor.register: received register request from {interaction.user}"
         )
         supabase: Client = self.extras["supabase"]
 
         await interaction.response.defer(ephemeral=True)
 
         # check if the password is correct
-        if password != os.getenv("JUDGE_PASSWORD"):
+        if password != os.getenv("SPONSOR_PASSWORD"):
             logging.warning(
-                f"Judge.register: {interaction.user} tried to register with the wrong password `{password}`"
+                f"Sponsor.register: {interaction.user} tried to register with the wrong password `{password}`"
             )
             await interaction.followup.send(
                 "The password you entered is incorrect. Please try again.",
@@ -34,10 +34,10 @@ class Judge(app_commands.Group):
 
         # Check if user exists in the database
         if check_if_user_exists(
-            client=supabase, username=interaction.user.name, table="judge"
+            client=supabase, username=interaction.user.name, table="sponsor"
         ):
             logging.warning(
-                f"Judge.register: {interaction.user} is already registered for the event"
+                f"Sponsor.register: {interaction.user} is already registered for the event"
             )
             await interaction.followup.send(
                 "You are already registered for the event", ephemeral=True
@@ -46,24 +46,28 @@ class Judge(app_commands.Group):
 
         # Register user
         user_roles = [role.name.lower() for role in interaction.user.roles]
-        logging.info(f"Judge.register: registering {interaction.user} for the event")
+        logging.info(f"Sponsor.register: registering {interaction.user} for the event")
         try:
-            supabase.table("judge").insert(
+            supabase.table("sponsor").insert(
                 {
                     "username": interaction.user.name,
                 }
             ).execute()
-            logging.info(f"Judge.register: {interaction.user} registered successfully")
-            # give judge role
-            if "judge" not in user_roles:
-                judge_role = discord.utils.get(interaction.guild.roles, name="Judge")
-                await interaction.user.add_roles(judge_role)
+            logging.info(
+                f"Sponsor.register: {interaction.user} registered successfully"
+            )
+            # give sponsor role
+            if "sponsor" not in user_roles:
+                sponsor_role = discord.utils.get(
+                    interaction.guild.roles, name="Sponsor"
+                )
+                await interaction.user.add_roles(sponsor_role)
             await interaction.followup.send(
                 "You have been registered for the event", ephemeral=True
             )
         except Exception as e:
             logging.error(
-                f"Judge.register: error while registering {interaction.user} for the event: {e}"
+                f"Sponsor.register: error while registering {interaction.user} for the event: {e}"
             )
             await interaction.followup.send(
                 "An error occurred while registering you for the event. Please try again later.",
@@ -72,12 +76,12 @@ class Judge(app_commands.Group):
 
 
 async def setup(client: discord.Client):
-    logging.info("Judge: registering slash command")
+    logging.info("Sponsor: registering slash command")
     load_dotenv()
     client.tree.add_command(
-        Judge(
-            name="judge",
-            description="Commands for judges",
+        Sponsor(
+            name="sponsor",
+            description="Commands for sponsors",
             extras={
                 "supabase": supabase_client,
             },
