bc4f3a1dc5b4fb0a87d91aa4678eab2ad3d4532a
menamerai <rphananh@gmail.com>
2024-08-25 02:41:51 +0000

    [fix] bool mistake

--- hazel/utils/users.py
+++ hazel/utils/users.py
@@ -18,10 +18,9 @@ def check_if_user_exists(
 def check_root_leaf_branch(roles: list[str], count_leaf: bool = True) -> bool:
     # check if there are exactly 1 root role, 1 leaf role, and 1 branch role
     root_roles = [role for role in roles if role in ROOT_ROLES]
-    leaf_roles = [role for role in roles if role in LEAF_ROLES]
     branch_roles = [role for role in roles if role in BRANCH_ROLES]
-    return (
-        len(root_roles) == 1
-        and ((len(leaf_roles) == 1) == count_leaf)
-        and len(branch_roles) == 1
-    )
+    if count_leaf:  # check for leaf roles
+        leaf_roles = [role for role in roles if role in LEAF_ROLES]
+        return len(root_roles) == 1 and len(leaf_roles) == 1 and len(branch_roles) == 1
+    else:
+        return len(root_roles) == 1 and len(branch_roles) == 1
